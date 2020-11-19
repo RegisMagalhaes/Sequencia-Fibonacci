73c9c74f69a11974f68426f5a8cba710f77a9ae2
Regis Leandro da Silva Ribeiro Magalhães <engenheiro.regis.magalhaes@gmail.com>
2020-11-19 02:08:18 -0300

    Este programa pergunta quantos termos o usuário pretende mostrar da sequencia Fibonacci

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Sequencia_Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("------------------------------------");
+            Console.WriteLine("Sequência Fibonacci");
+            Console.WriteLine("------------------------------------");
+
+            Console.Write("Quantos termos você quer mostrar?");
+            int num = int.Parse(Console.ReadLine());
+
+            int t1 = 0;
+            int t2 = 1;
+
+            Console.WriteLine($"{t1}");
+            Console.WriteLine($"{t2}");
+
+            int cont = 3;
+            while(cont <= num) {
+                int t3 = t1 + t2;
+                Console.WriteLine($"{t3}");
+                t1 = t2;
+                t2 = t3;
+                cont++;
+
+            }
+            Console.WriteLine("FIM");
+            
+        }
+    }
+}
