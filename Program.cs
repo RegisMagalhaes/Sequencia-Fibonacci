93ddde7a312abb5f475154cc3dd83b7a58d2623a
Regis Leandro da Silva Ribeiro Magalhães <engenheiro.regis.magalhaes@gmail.com>
2020-11-19 22:03:02 -0300

    Este programa calcula a sequencia fibonacci até encontrar o valor maior ou igual a 500

--- Program.cs
+++ Program.cs
@@ -5,6 +5,10 @@ namespace Sequencia_Fibonacci
     class Program
     {
         static void Main(string[] args)
+
+        //A série de Fibonacci é formada pela seqüência 0,1,1,2,3,5,8,13,21,34,55,...
+        //Regis, pesquise como funciona o algoritmo Fibonacci.
+        //Faça um programa que gere a série até que o valor seja maior que 500.
         {
             Console.WriteLine("------------------------------------");
             Console.WriteLine("Sequência Fibonacci");
@@ -20,14 +24,31 @@ namespace Sequencia_Fibonacci
             Console.WriteLine($"{t2}");
 
             int cont = 3;
+
+            // for (int cont = 3; cont < num; cont++)
+            // {   
+            //     int t3 = t1 + t2;
+            //     if(t3 >= 500){
+            //         break;
+            //     }
+            //     Console.WriteLine($"{t3}");
+            //     t1 = t2;
+            //     t2 = t3;
+                
+            // }
+        
             while(cont <= num) {
                 int t3 = t1 + t2;
+                if(t3 >= 500){
+                    break;
+                }
                 Console.WriteLine($"{t3}");
                 t1 = t2;
                 t2 = t3;
                 cont++;
-
+                
             }
+            
             Console.WriteLine("FIM");
             
         }
